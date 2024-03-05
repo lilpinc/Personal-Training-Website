8e36701b4fa69c474af7671eb6e3e709048af7aa
lilpinc <133043171+lilpinc@users.noreply.github.com>
2024-03-05 16:49:00 -0500

    media queried team page

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -2,19 +2,23 @@ export default function Team() {
     return (
         <>
             <section className="info howtogetstarted">
-                <header className="about">
-                    <h2 id="name">Anna-Britta Pincus</h2>
-                </header>
-                <p id="founder">Founder and Head Strength & Conditioning Specialist</p>
-                <br  />
+                <div className="about">
+                    <header>
+                        <h2 id="name">Anna-Britta Pincus</h2>
+                    </header>
+                    <p id="founder">Founder and Head Strength & Conditioning Specialist</p>
+                    <br />
+                </div>
                 <div className="bio">
-                <img className="awc" src="Pictures/Deadlifting.PNG" alt="anna" />
-                <div id="bio">
-                    <p>Anna-Britta Pincus, NSCA-CSCS, NASM-CNC,
-                        has been involved in fitness for most of her life. From a young age she took part in multiple sports and recreational activities including soccer, skiing, cross country & track, and club ultimate frisbee. Since graduating with a B.S. in Biology from Virginia Tech Polytechnic and State University in 2017, Anna has pursued a career in the fitness industry to help others improve their health through exercise prescription. In 2019 Anna received her M.S. in Exercise Science at Old Dominion University. During her graduate program Anna taught Human Anatomy and Physiology Lab at Tidewater Community College and completed a strength and conditioning internship with CHKD Sports Performance in Virginia Beach. After receiving her masters in Exercise Science, Anna went on to complete her Strength and Conditioning Specialist Certification through NSCA in 2020 and a Nutrition Coach certification through NASM in 2022. </p>
-
-                    <p>Anna's training experience includes drills in acceleration, speed, agility, and plyometrics, as well as endurance and resistance programming. While her personal passion lies in athletic conditioning, Anna greatly enjoys programming for the general public and those suffering from chronic pain and/or injury. In 2020 Anna joined Advanced Wellness Centre in Richmond, Virginia where she guided multiple patients through pain relieving mobility and strength training programs. Working together with the chiropractors and RDA staff, she prescribes tailored workouts that were specific to each patients fitness level, personal exercise goal, and movement assessment. Training with Anna is a holistic experience, she addresses multiple aspects of a clients fitness including mental health, sleep, nutrition, mobility, flexibility, balance, and strength. She consistently monitors the clients progress and alters all exercises to fit their movement capabilities. Overall, Anna's primary goal as a strength and conditioning specialist is to promote a healthy lifestyle for each client through functional movement and exercise. When not working Anna can be found either in the gym lifting or enjoying the outdoors climbing, hiking, or trail running.</p></div>
+                    <div>
+                        <img className="awc" src="Pictures/Deadlifting.PNG" alt="anna" />
                     </div>
+                    <div id="bio">
+                        <p>Anna-Britta Pincus, NSCA-CSCS, NASM-CNC,
+                            has been involved in fitness for most of her life. From a young age she took part in multiple sports and recreational activities including soccer, skiing, cross country & track, and club ultimate frisbee. Since graduating with a B.S. in Biology from Virginia Tech Polytechnic and State University in 2017, Anna has pursued a career in the fitness industry to help others improve their health through exercise prescription. In 2019 Anna received her M.S. in Exercise Science at Old Dominion University. During her graduate program Anna taught Human Anatomy and Physiology Lab at Tidewater Community College and completed a strength and conditioning internship with CHKD Sports Performance in Virginia Beach. After receiving her masters in Exercise Science, Anna went on to complete her Strength and Conditioning Specialist Certification through NSCA in 2020 and a Nutrition Coach certification through NASM in 2022. </p>
+
+                        <p>Anna's training experience includes drills in acceleration, speed, agility, and plyometrics, as well as endurance and resistance programming. While her personal passion lies in athletic conditioning, Anna greatly enjoys programming for the general public and those suffering from chronic pain and/or injury. In 2020 Anna joined Advanced Wellness Centre in Richmond, Virginia where she guided multiple patients through pain relieving mobility and strength training programs. Working together with the chiropractors and RDA staff, she prescribes tailored workouts that were specific to each patients fitness level, personal exercise goal, and movement assessment. Training with Anna is a holistic experience, she addresses multiple aspects of a clients fitness including mental health, sleep, nutrition, mobility, flexibility, balance, and strength. She consistently monitors the clients progress and alters all exercises to fit their movement capabilities. Overall, Anna's primary goal as a strength and conditioning specialist is to promote a healthy lifestyle for each client through functional movement and exercise. When not working Anna can be found either in the gym lifting or enjoying the outdoors climbing, hiking, or trail running.</p></div>
+                </div>
                 <div className="onlinelinks">
                     <ul className="onlinelinks">
                         <li id="instagram"><a href="https://www.instagram.com/abps.fc/" target="_blank"><i className="fa-brands fa-instagram"></i></a></li>
